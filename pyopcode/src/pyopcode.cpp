5397893410a76742d3b72a22f2a8b27e905318b0
Eelco Hoogendoorn <hoogendoorn.eelco@gmail.com>
2016-05-31 15:12:31 +0200

    included Ice

--- pyopcode/src/pyopcode.cpp
+++ pyopcode/src/pyopcode.cpp
@@ -21,9 +21,9 @@ public:
         vertices(vertices),
         triangles(triangles)
     {
-        if (vertices.shape[1] != 3)
+        if (vertices.shape()[1] != 3)
             throw python_exception("vertices needs to be an [n, 3] array");
-        if (triangles.shape[1] != 3)
+        if (triangles.shape()[1] != 3)
             throw python_exception("triangles needs to be an [n, 3] array");
     }
 
@@ -34,7 +34,10 @@ public:
         opcMeshAccess->SetNbTriangles(triangles.size());
         opcMeshAccess->SetNbVertices(vertices.size());
 
-        opcMeshAccess->SetPointers(triangles.data(), vertices.data());
+        opcMeshAccess->SetPointers(
+            triangles.view<IceMaths::IndexedTriangle>().data(),
+            vertices.view <IceMaths::Point          >().data()
+        );
 
         //Tree building settings
         OPCC.mIMesh = opcMeshAccess;
@@ -51,7 +54,7 @@ public:
 
 template<typename real_t, typename index_t>
 class MeshCollision {
-    typedef Mesh<real_t, index_t> mesh_t
+    typedef Mesh<real_t, index_t> mesh_t;
     typedef ndarray<real_t, 2> affine_t;
 
 	const mesh_t mesh1;
@@ -78,10 +81,10 @@ public:
 
 		// Collision query
     	Opcode::AABBTreeCollider TC;
-		const bool IsOk = TC.Collide(ColCache, affine1.data(), affine2.data());
+		const bool IsOk (TC.Collide(ColCache, (IceMaths::Matrix4x4*)affine1.data(), (IceMaths::Matrix4x4*)affine2.data()));
 
 		// Get collision status => if true, objects overlap
-		const bool Status = TC.GetContactStatus();
+		const bool Status (TC.GetContactStatus());
 
 		int NbPairs; //number of colliding pairs
 
@@ -89,7 +92,7 @@ public:
 			// Number of colliding pairs and list of pairs
 			NbPairs = TC.GetNbPairs();
 			//printf("colliding: %d\n", NbPairs);
-			p = TC.GetPairs();
+//			p = TC.GetPairs();
 		}
 		else {
 			NbPairs = 0;
